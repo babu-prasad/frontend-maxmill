31b8019aac571b2e127013064f61ec4798b9c26a
babu-prasad <babuprasadsoft@gmail.com>
2023-07-05 11:08:39 +0530

    user changed

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,7 +3,7 @@ import UserList from "../components/UserList";
 
 const Users = () => {
     //const USERS=[];
-  const USERS = [{ id: "u1", name: "max-millan", image: "", places: 3 }];
+  const USERS = [{ id: "u1", name: "max-well-bro", image: "", places: 3 }];
   return <UserList items={USERS} />;
 };
 export default Users;
